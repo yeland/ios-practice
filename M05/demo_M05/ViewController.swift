9575a182b3e7cb203c2a6a401ead51c3e06a9808
yeland <wlx100715@163.com>
2019-11-18 18:03:13 +0800

    [weilinxiao] feat: practice debugging in M05

---
+++ M05/demo_M05/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  demo_M05
+//
+//  Created by Xin Guo on 2019/11/8.
+//  Copyright © 2019 personal.emagrorrim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  @IBOutlet weak var containerView: UIView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var contentLabel: UILabel!
+  @IBOutlet weak var imageContentView: UIView!
+
+  var viewModel: ViewModel!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    viewModel = ViewModel(updateView: {
+      [weak self] in
+      self?.containerView.backgroundColor = .systemBlue
+      self!.doSomething()
+    })
+    viewModel.update()
+  }
+
+  func doSomething() {
+  }
+}
+
+class ViewModel {
+  let updateView: () -> Void
+
+  init(updateView: @escaping () -> Void) {
+    self.updateView = updateView
+  }
+
+  func update() {
+    self.updateView()
+  }
+}
+
