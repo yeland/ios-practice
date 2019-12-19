2bc9a87834f18a5f9a18a9243addf31dc3456b09
yeland <wlx100715@163.com>
2019-12-19 11:31:38 +0800

    [weilinxiao] [moment] feat: test the logic of adding comment

--- Advanced/DynamicContentDemo/dynamic-content-demoTests/MomentsViewModelSpec.swift
+++ Advanced/DynamicContentDemo/dynamic-content-demoTests/MomentsViewModelSpec.swift
@@ -12,19 +12,43 @@ import Nimble
 
 class MomentsViewModelSpec: QuickSpec {
   override func spec() {
-    let moment = Moment(content: "", images: [Image(url: ""), Image(url: "")], sender: Sender(username: "", nick: "", avatar: ""), comments: [])
-    let momentWithoutSender = Moment(content: "", images: [], sender: nil, comments: [])
-    let momentWithoutContent = Moment(content: nil, images: [Image(url: ""), Image(url: "")], sender: Sender(username: "", nick: "", avatar: ""), comments: [])
-    let momentWithoutImage = Moment(content: "", images: nil, sender: Sender(username: "", nick: "", avatar: ""), comments: [])
-    let invalidMoment = Moment(content: nil, images: nil, sender: Sender(username: "", nick: "", avatar: ""), comments: [])
+    let moment = Moment(
+      content: "",
+      images: [Image(url: ""), Image(url: "")],
+      sender: Sender(username: "", nick: "", avatar: ""),
+      comments: [Comment(content: "hello", sender: Sender(username: "", nick: "", avatar: ""))]
+    )
+    let momentWithoutSender = Moment(
+      content: "",
+      images: [],
+      sender: nil,
+      comments: []
+    )
+    let momentWithoutContent = Moment(
+      content: nil,
+      images: [Image(url: ""), Image(url: "")],
+      sender: Sender(username: "", nick: "", avatar: ""),
+      comments: []
+    )
+    let momentWithoutImage = Moment(
+      content: "", images: nil,
+      sender: Sender(username: "", nick: "", avatar: ""),
+      comments: nil
+    )
+    let invalidMoment = Moment(
+      content: nil, images: nil,
+      sender: Sender(username: "", nick: "", avatar: ""),
+      comments: []
+    )
+    
     let allMoments = [moment, moment, moment, moment, momentWithoutImage, moment, moment, momentWithoutSender, momentWithoutSender, moment, moment, momentWithoutContent, moment, invalidMoment, moment, moment, invalidMoment, moment]
     
     describe("getValidMoments") {
       context("should get all valid comments") {
         it("return valid comments") {
-          let momentviewModel = MomentsViewModel()
-          momentviewModel.getValidMoments(allMoments: allMoments)
-          expect(momentviewModel.allValidMoments.count) == 14
+          let momentViewModel = MomentsViewModel()
+          momentViewModel.getValidMoments(allMoments: allMoments)
+          expect(momentViewModel.allValidMoments.count) == 14
         }
       }
     }
@@ -32,19 +56,19 @@ class MomentsViewModelSpec: QuickSpec {
     describe("getInitialMoments") {
       context("should get initial moments") {
         it("return 5 moments when count of moments is more than 5") {
-          let momentviewModel = MomentsViewModel()
-          momentviewModel.getValidMoments(allMoments: allMoments)
-          momentviewModel.getInitialMoments()
-          expect(momentviewModel.moments.count) == 5
-          expect(momentviewModel.moments[4].images).to(beNil())
+          let momentViewModel = MomentsViewModel()
+          momentViewModel.getValidMoments(allMoments: allMoments)
+          momentViewModel.getInitialMoments()
+          expect(momentViewModel.moments.count) == 5
+          expect(momentViewModel.moments[4].images).to(beNil())
         }
         
         it("return all moments when count of moments is less than 5") {
-          let momentviewModel = MomentsViewModel()
+          let momentViewModel = MomentsViewModel()
           let allMoments = [moment, moment, moment]
-          momentviewModel.getValidMoments(allMoments: allMoments)
-          momentviewModel.getInitialMoments()
-          expect(momentviewModel.moments.count) == 3
+          momentViewModel.getValidMoments(allMoments: allMoments)
+          momentViewModel.getInitialMoments()
+          expect(momentViewModel.moments.count) == 3
         }
       }
     }
@@ -52,21 +76,21 @@ class MomentsViewModelSpec: QuickSpec {
     describe("loadMore") {
       context("should load more moments") {
         it("load more 5 moments when remaining comments' count is more than 5") {
-          let momentviewModel = MomentsViewModel()
-          momentviewModel.getValidMoments(allMoments: allMoments)
-          momentviewModel.getInitialMoments()
-          momentviewModel.loadMore()
-          expect(momentviewModel.moments.count) == 10
+          let momentViewModel = MomentsViewModel()
+          momentViewModel.getValidMoments(allMoments: allMoments)
+          momentViewModel.getInitialMoments()
+          momentViewModel.loadMore()
+          expect(momentViewModel.moments.count) == 10
         }
         
         it("load remaining comments when remaining comments' count is less than 5") {
-          let momentviewModel = MomentsViewModel()
-          momentviewModel.getValidMoments(allMoments: allMoments)
-          momentviewModel.getInitialMoments()
-          momentviewModel.loadMore()
-          expect(momentviewModel.moments.count) == 10
-          momentviewModel.loadMore()
-          expect(momentviewModel.moments.count) == 14
+          let momentViewModel = MomentsViewModel()
+          momentViewModel.getValidMoments(allMoments: allMoments)
+          momentViewModel.getInitialMoments()
+          momentViewModel.loadMore()
+          expect(momentViewModel.moments.count) == 10
+          momentViewModel.loadMore()
+          expect(momentViewModel.moments.count) == 14
         }
       }
     }
@@ -74,13 +98,43 @@ class MomentsViewModelSpec: QuickSpec {
     describe("isAllLoaded") {
       context("should check if all moments is loaded") {
         it("return if all moments is loaded") {
-          let momentviewModel = MomentsViewModel()
-          momentviewModel.getValidMoments(allMoments: allMoments)
-          momentviewModel.getInitialMoments()
-          momentviewModel.loadMore()
-          expect(momentviewModel.isAllLoaded()) == false
-          momentviewModel.loadMore()
-          expect(momentviewModel.isAllLoaded()) == true
+          let momentViewModel = MomentsViewModel()
+          momentViewModel.getValidMoments(allMoments: allMoments)
+          momentViewModel.getInitialMoments()
+          momentViewModel.loadMore()
+          expect(momentViewModel.isAllLoaded()) == false
+          momentViewModel.loadMore()
+          expect(momentViewModel.isAllLoaded()) == true
+        }
+      }
+    }
+    
+    describe("addComment") {
+      context("should add comment to corresponding moment") {
+        it("append comment when there are comments") {
+          let momentViewModel = MomentsViewModel()
+          momentViewModel.getValidMoments(allMoments: allMoments)
+          momentViewModel.getInitialMoments()
+          momentViewModel.addComment(row: 0, comment: "world")
+          let comments = momentViewModel.allValidMoments[0].comments
+          expect(comments).notTo(beNil())
+          if let comments = comments {
+            expect(comments.count) == 2
+            expect(comments[1].content) == "world"
+          }
+        }
+        
+        it("set comment when there is no comment") {
+          let momentViewModel = MomentsViewModel()
+          momentViewModel.getValidMoments(allMoments: allMoments)
+          momentViewModel.getInitialMoments()
+          momentViewModel.addComment(row: 4, comment: "world")
+          let comments = momentViewModel.allValidMoments[4].comments
+          expect(comments).notTo(beNil())
+          if let comments = comments {
+            expect(comments.count) == 1
+            expect(comments[0].content) == "world"
+          }
         }
       }
     }
